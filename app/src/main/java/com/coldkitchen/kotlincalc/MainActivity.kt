ede668c6b5431ce560e6764c136b93da6069b0fa
MaxPaws <45854378+MaxPaws@users.noreply.github.com>
2021-03-26 19:59:41 +0300

    Initial commit

---
+++ app/src/main/java/com/coldkitchen/kotlincalc/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.coldkitchen.kotlincalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun OnDigit(view: View){
+
+    }
+}
