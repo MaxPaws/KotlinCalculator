8c915e34f343565f4dcc0ddef2ad8fefaa7e0813
MaxPaws <45854378+MaxPaws@users.noreply.github.com>
2021-03-28 16:48:30 +0300

    Added buttons functionality. Добавлена функциональность для кнопок.

--- app/src/main/java/com/coldkitchen/kotlincalc/MainActivity.kt
+++ app/src/main/java/com/coldkitchen/kotlincalc/MainActivity.kt
@@ -3,6 +3,8 @@ package com.coldkitchen.kotlincalc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun OnDigit(view: View){
+        tvInput.append((view as Button).text)
+    }
 
+    fun onClear(view: View){
+        tvInput.text=""
     }
 }
